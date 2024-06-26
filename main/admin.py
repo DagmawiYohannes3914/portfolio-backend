68faf60cc840989b991629c51318836310e55a9a
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-06-26 08:44:09 +0300

    registering models in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Introduction, About, Project, Resume, Contact, Blog
 
-# Register your models here.
+admin.site.register(Introduction)
+admin.site.register(About)
+admin.site.register(Project)
+admin.site.register(Resume)
+admin.site.register(Contact)
+admin.site.register(Blog)
